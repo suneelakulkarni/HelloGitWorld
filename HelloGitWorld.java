1a86aaa079260b7e5421754a225bdd613440c97c
Sunil Kulkarni <sunil_kulkarni@persistent.co.in>
2012-07-31 10:40:07 +0530

    First Commit

---
+++ HelloGitWorld.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * @author Sunil Kulkarni
+ *
+ */
+public class HelloGitWorld {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("HelloGitWorld");
+  }
+
+}
